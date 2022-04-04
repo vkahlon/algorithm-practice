8839a7fefae4e75fa798f999bfe676ab09c0ebd6
vkahlon <vickram.kahlon@gmail.com>
2022-04-04 11:20:06 -0700

    Completed Challenge

---
+++ even-only/even-only.js
@@ -0,0 +1,10 @@
+function solution(n) {
+  const array = String(n).split('');
+  const num = [];
+  array.forEach(e => {
+    num.push(Number.parseInt(escape));
+
+  });
+  return !!num.every(num => num % 2 === 0);
+}
+solution();
