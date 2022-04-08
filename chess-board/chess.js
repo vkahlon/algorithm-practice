9b14883c709cbd9f90d398930b77044defd51aed
vkahlon <vickram.kahlon@gmail.com>
2022-04-08 11:35:10 -0700

    completed challenge

---
+++ chess-board/chess.js
@@ -0,0 +1,5 @@
+function solution(cell1, cell2) {
+  return cell1.split('').reduce((a, b) =>
+    Math.abs(a.charCodeAt(0) - 'A'.charCodeAt(0) + 1 - b) % 2) === cell2.split('').reduce((a, b) => Math.abs(a.charCodeAt(0) - 'A'.charCodeAt(0) + 1 - b) % 2);
+}
+solution();
